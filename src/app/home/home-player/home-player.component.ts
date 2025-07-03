6338107b5a4c178ca7e310426a552348bb674b0b
Ariana-Rodriguez-suda <amrodriguez@sudamericano.edu.ec>
2025-07-03 16:31:55 -0500

    20

--- src/app/home/home-player/home-player.component.ts
+++ src/app/home/home-player/home-player.component.ts
@@ -1,15 +1,17 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-player',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './home-player.component.html',
   styleUrls: ['./home-player.component.css']
 })
 export class HomePlayerComponent {
   constructor(private router: Router) {}
-
+  backgroundUrl = 'assets/images/background_home.jpg';
   goToTeacher() {
     this.router.navigate(['/home-teacher']);
   }
