6bbc425092b84406355ab3657a438538f24f37e8
Ariana-Rodriguez-suda <amrodriguez@sudamericano.edu.ec>
2025-07-02 21:55:52 -0500

    19

--- src/app/home/home-teacher/home-teacher.component.ts
+++ src/app/home/home-teacher/home-teacher.component.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home-teacher',
   standalone: true,
+  imports: [CommonModule],
   templateUrl: './home-teacher.component.html',
   styleUrls: ['./home-teacher.component.css']
 })
 export class HomeTeacherComponent {
   constructor(private router: Router) {}
+backgroundUrl = 'assets/images/background_home.jpg';
+
 
   goToPlayer() {
     this.router.navigate(['/home-player']);
