605ecc135f7bcaf56df8591c9413eb716e5e91e8
Ariana-Rodriguez-suda <amrodriguez@sudamericano.edu.ec>
2025-08-04 20:05:25 -0500

    56

--- src/app/home/home-player/home-player.component.ts
+++ src/app/home/home-player/home-player.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { BotonComponent } from '../../common/button/button.component';
 
 @Component({
   selector: 'app-home-player',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BotonComponent],
   templateUrl: './home-player.component.html',
   styleUrls: ['./home-player.component.css']
 })
--- src/app/home/home-teacher/home-teacher.component.ts
+++ src/app/home/home-teacher/home-teacher.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { CommonModule } from '@angular/common';
+import { BotonComponent } from '../../common/button/button.component';
 
 @Component({
   selector: 'app-home-teacher',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, BotonComponent],
   templateUrl: './home-teacher.component.html',
   styleUrls: ['./home-teacher.component.css']
 })
